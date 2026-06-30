6118016614898aa392bca074300db2f44b5237e7
faizanhussain0601 <faizanhussain0601@gmail.com>
2026-06-30 10:22:26 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let score = true
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let ValueInNumber = Number(score)
-console.log(typeof ValueInNumber)
-console.log(ValueInNumber)
+// console.log(typeof ValueInNumber)
+// console.log(ValueInNumber)
 
 // "33" => 33
 // "33abc" => NaN
@@ -15,7 +15,7 @@ let IsLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(IsLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true ; 0 => false
 // "" => false
@@ -25,5 +25,42 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// "33 " => 33
+// string to Int
+
+// **************** Operations *****************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**3)
+// console.log(2%3)
+
+let str1 = "Hello"
+let str2 = " Faizan"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(3+4*5%3)
+
+console.log(+true)
+console.log(+"")
+
+let gameCounter = 100
+// ++gameCounter
+gameCounter++
+console.log(gameCounter)

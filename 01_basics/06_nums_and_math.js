b8b960b9ee28dc00157fcd2c6e594cf3b26e31d0
faizanhussain0601 <faizanhussain0601@gmail.com>
2026-07-02 11:42:23 +0000

    nums and maths

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -16,3 +16,19 @@ const hundreds = 1000000
 
 // +++++++++++++++++++++Maths+++++++++++++++++++++++++++++++++++
 
+console.log(Math);
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.9))
+console.log(Math.min(1,2,6,8))
+console.log(Math.max(1,2,6,8))
+
+console.log(Math.random()*10)
+console.log((Math.random()*10)+1)
+console.log((Math.random()*10)+1)
+
+const mini = 10
+const maxi = 20
+
+console.log(Math.floor(Math.random() * (maxi-mini+1)) + mini)

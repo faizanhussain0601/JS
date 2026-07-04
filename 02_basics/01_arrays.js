d55320e9c834f32c71bb070fb4e7f65de183d615
faizanhussain0601 <faizanhussain0601@gmail.com>
2026-07-04 13:21:09 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+// aarray
+
+const myArray = [0,1,2,3,4,5]
+const myHero = ["Mom","Dad"]
+
+const myArray2 = new Array(1,2,3,4)
+// console.log(myArray2[0])
+
+// array methods
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+
+// myArray.unshift(8)
+// myArray.shift()
+
+// console.log(myArray.includes(8));
+// console.log(myArray.includes(2));
+
+// console.log(myArray.indexOf(4))
+
+// const newArr = [0,1,2,3,4,5]
+
+// console.log(myArray)
+
+// console.log(newArr.join())
+
+// Slice Splice
+
+console.log("A",myArray)
+
+const mynewArr1 = myArray.slice(1,3)
+console.log(mynewArr1)
+console.log("B",myArray)
+
+const mynewArr2 = myArray.splice(1,3)
+console.log(mynewArr2)
+console.log("C",myArray) 

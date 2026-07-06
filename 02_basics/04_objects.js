a0d99a007983d68b261a2e025274ca770e6cc402
faizanhussain0601 <faizanhussain0601@gmail.com>
2026-07-06 12:51:03 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -54,3 +54,31 @@ console.log(Object.values(tinderUser))
 console.log(Object.entries(tinderUser))
 
 console.log(tinderUser.hasOwnProperty("isLoggedIn")) // property check usme yeh available hai ya nahi
+
+
+const course = {
+      coursename: "JS learning course",
+      price: "999",
+      courseInstructor: "Hitesh"
+}
+
+// course.constructor iski jagah niche woh use karenge easy hai
+const {courseInstructor: instructor} = course // yaha ham iski rename kar sakte hai
+
+console.log(instructor)
+
+// JSON hai yeh 
+// {
+//     name: "faizan",
+//     coursename: "JavaScript learning",
+//     courseprice: "free"
+
+// } 
+
+
+// esa bhi aata hai json array 
+// [
+//     {},
+//     {},
+//     {}
+// ]

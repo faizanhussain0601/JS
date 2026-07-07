8e003291f386c648a1eb1d6af70b949753b5d79d
faizanhussain0601 <faizanhussain0601@gmail.com>
2026-07-07 09:28:54 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,3 +33,35 @@ function LoginUserMeesage(username){
 }
 
 // console.log(LoginUserMeesage(""))
+
+function calculatoCartprice(...num1){
+    return num1
+}
+function calculatoCartprice2(val1,val2,...num1){
+    return num1
+}
+// console.log(calculatoCartprice2(200,300,400,2000))
+
+const user = {
+    username: "Faizan",
+    Price: 400
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.Price}`)
+}
+
+// handleObject(user)
+handleObject({
+    username: "Sam khan",
+    Price: 299
+})
+
+const myNewArray = [200,300,400,500]
+
+function returnSecondValue(getarray){
+    return getarray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200,500,700,800,1000]))

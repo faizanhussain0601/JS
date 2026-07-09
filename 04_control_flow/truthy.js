eb06095fcd1192558aef44736b3b086916d2d9ac
faizanhussain0601 <faizanhussain0601@gmail.com>
2026-07-09 12:35:07 +0000

    control flow

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,15 @@
+const Useremail = "faizan@google.com"
+
+if(Useremail){
+    console.log("Got user email")
+}
+else{
+    console.log("Dont have user email")
+}
+
+// falsy value
+//  false , 0 , -0 , BigInt 0n, "", null , undefined, NaN
+
+// truthy value
+// "0", "false", " " ,[], {}, function(){}
+// Note inside any string " " value is truthy

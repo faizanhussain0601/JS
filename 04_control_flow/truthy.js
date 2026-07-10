ff6bdac2ee57ddfdd081e9b300524f8fef878ccc
faizanhussain0601 <faizanhussain0601@gmail.com>
2026-07-10 15:45:05 +0000

    control flow

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -13,3 +13,31 @@ else{
 // truthy value
 // "0", "false", " " ,[], {}, function(){}
 // Note inside any string " " value is truthy
+
+const Useremail2 = []
+
+if(Useremail2.length ===0){
+    console.log("Array is empty")
+}
+
+const emptyobj = {}
+
+if(Object.keys(emptyobj).length === 0){
+    console.log("Object is empty");
+    
+}
+
+// Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+//  val1 =  5 ?? 10
+//  val1 =  null ?? 10
+//  val1 =  undefined ?? 10
+
+ val1 = null ?? 10 ?? 20 
+
+ console.log(val1)
+
+ // Ternary Operator 
+
+ condition ? true : false;

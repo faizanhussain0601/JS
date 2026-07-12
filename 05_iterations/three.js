b5571bfe513f834005ec823aca7411b465f63653
faizanhussain0601 <faizanhussain0601@gmail.com>
2026-07-12 09:33:18 +0000

    map

---
+++ 05_iterations/three.js
@@ -0,0 +1,30 @@
+// for of
+
+// ["","",""]
+// [{},{},{}]
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    // console.log(num)
+}
+
+const greetings = "Hello World"
+
+for (const greet of greetings) {
+    // console.log(greet)
+}
+
+// MAPS
+
+const map = new Map()
+
+map.set('IN','INDIA')
+map.set('USA','United States of America')
+map.set('FR','France')
+
+// console.log(map)
+
+for (const  [key,value] of map) {
+    console.log(`${key} :- ${value}`)
+}

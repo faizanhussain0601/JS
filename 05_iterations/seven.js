330b842f2a5a8ed13f6a413af8d6170e7724e444
faizanhussain0601 <faizanhussain0601@gmail.com>
2026-07-15 10:47:10 +0000

    filter and map

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,1 +1,12 @@
 const myNumers = [1,2,3,4,5,6,7,8,9,10]
+
+const newNums  = myNumers.map((nums)=> {return nums + 10})
+
+
+const newNums2 = myNumers
+               .map((nums) => nums * 10)
+               .map((nums) => nums + 1)
+               .filter((nums) => nums >= 40)
+
+console.log(newNums2)
+

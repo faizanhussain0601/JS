860cbe09e898906ad0cc131ff21fa435c2436fb2
Faizan Hussain <faizanhussain0601@gmail.com>
2026-07-24 23:37:45 +0530

    project 1

--- 07_Projects/01-colorChanger/chaiaurcode.js
+++ 07_Projects/01-colorChanger/chaiaurcode.js
@@ -2,7 +2,7 @@ const buttons  = document.querySelectorAll('.button');
 const body = document.querySelector("body");
 
 buttons.forEach( function(button){
-       button.addEventListener('click',function(){
-        
+       button.addEventListener('click',function(e){
+         
        })
 })
